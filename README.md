4cca41325c512049406e1ac61c2fc54c5f50b63e
Siddharth Vishwanathan <siddvish21@gmail.com>
2023-04-29 15:34:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Minecraft Player List Tracker
+
+This script is designed to monitor changes to the player list on an Aternos Minecraft server, and record them in an Excel sheet. The script checks the player list every 10 seconds, and if a new player has joined or left the server, it updates the data in the Excel sheet.
+
+## Prerequisites
+
+To use this script, you will need:
+
+- Python 3 installed on your machine
+- Aternos account credentials to access your server's player list
+- An Excel sheet to record the player list data
+
+
+## Installation
+
+## Installation
+
+1. Clone this repository using `git clone https://github.com/siddvish21/minecraft-player-list-tracker.git`
+2. Navigate to the cloned repository using `cd minecraft-player-list-tracker`
+3. Install virtualenv using `pip install virtualenv`
+4. Create a new virtual environment by running `python3 -m venv <Your virtual environment name>`
+5. Activate the virtual environment by running `<Your virtual environment name>\Scripts\activate` (Windows) or `source env/bin/activate` (macOS/Linux)
+6. Install the dependencies by running `pip install -r requirements.txt`
+7. Update the Aternos credentials and Excel sheet name in the file
+8. Run the script using `python main.py`
+
+## Usage
+
+The script will continuously monitor the player list on the Aternos Minecraft server and update the Excel sheet whenever a new player joins or an existing player leaves the server. The Excel sheet will be updated with the following columns:
+
+- Player name
+- Leave/Join
+- Time
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgements
+
+- [python-atemos](https://github.com/aternosorg/python-atemos) - Python library for Aternos API.
+- [openpyxl](https://openpyxl.readthedocs.io/) - Python library to work with Excel files.
+- [python-dotenv](https://github.com/theskumar/python-dotenv) - Python library to read variables from `.env` file.
---
+++ main.py
@@ -0,0 +1,65 @@
+from python_aternos import Client
+import os
+import openpyxl
+import datetime
+from time import sleep
+from pytz import timezone
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def currentTime():
+    #Change the timezone according to yours
+    tz = timezone("Asia/Kolkata")
+    date = datetime.datetime.now(tz)
+    return date.strftime("%d-%m-%Y %H:%M:%S")
+
+
+def check_player():
+  
+  time = []
+  leave_join = []
+  playername = []
+
+  #Update your aternos credentials in the .env file
+  
+  api = Client.from_credentials(os.getenv('ATERNOS_USERNAME'), os.getenv('ATERNOS_PASSWORD'))
+  servers = api.list_servers()
+  server = servers[2]
+  orgi_player_list = set(server.players_list)
+  while True:
+    sleep(10)
+    server.fetch()
+    current_player_list = set(server.players_list)
+    new_players = list(current_player_list - orgi_player_list)
+    left_players = list(orgi_player_list - current_player_list)
+    if new_players:
+      print(f'New player joined {new_players} at {currentTime()}')
+      time.append(currentTime())
+      playername.append(''.join(new_players))
+      leave_join.append('Joined')
+      orgi_player_list = current_player_list
+      data = list(zip(playername, leave_join, time))
+      wb = openpyxl.load_workbook('server-player-list.xlsx')
+      sheet = wb.active
+      sheet["A1"] = "Player name"
+      sheet["B1"] = "Leave/Join"
+      sheet["C1"]="Time"
+      for row in data:
+        sheet.append(row)
+      wb.save('server1.xlsx')
+    elif left_players:
+      print(f'Player left: {left_players} at {currentTime()}')
+      time.append(currentTime())
+      playername.append(''.join(left_players))
+      leave_join.append('Left')
+      orgi_player_list = current_player_list
+      data = list(zip(playername, leave_join, time))
+      wb = openpyxl.load_workbook('server1.xlsx')
+      sheet = wb.active
+      sheet.append(('playername', 'Leave/Join', 'Time'))
+      for row in data:
+        sheet.append(row)
+        wb.save('server-player-list.xlsx')
+
+check_player()
